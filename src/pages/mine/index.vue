522a674c8461fcc3a6dd2cddbc76d8ccf150ac61
student <student@hubu.edu.cn>
2019-03-25 17:31:51 +0800

    second

---
+++ src/pages/mine/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+     <div>
+     <p>我想放假！！！！</p>
+     </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.log-list {
+  display: flex;
+  flex-direction: column;
+  padding: 40rpx;
+}
+
+.log-item {
+  margin: 10rpx;
+}
+</style>
