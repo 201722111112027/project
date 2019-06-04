8c6a15ab2756a888770ab60e81b4372d7bfe6afe
gxm <2240428093@qq.com>
2019-06-04 23:43:37 +0800

    sub2

--- src/pages/book5/shanhezhishu/index.vue
+++ src/pages/book5/shanhezhishu/index.vue
@@ -4,30 +4,30 @@
     <view class="setting">
       <p>我的文化山河</p>
     <i-cell-group class="setting1">
-    <i-cell title="第一章" is-link url="/pages/book5/mulu/1/main"></i-cell>
-    <i-cell title="第二章" is-link url="/pages/book5/mulu/2/main"></i-cell>
-    <i-cell title="第三章" is-link url="/pages/book5/mulu/3/main"></i-cell>
-    <i-cell title="第四章" is-link url="/pages/book5/mulu/4/main"></i-cell>
-    <i-cell title="第五章" is-link url="/pages/book5/mulu/5/main"></i-cell>
-    <i-cell title="第六章" is-link url="/pages/book5/mulu/6/main"></i-cell>
-    <i-cell title="第七章" is-link url="/pages/book5/mulu/7/main"></i-cell>
-    <i-cell title="第八章" is-link url="/pages/book5/mulu/8/main"></i-cell>
+    <i-cell title="第一章" is-link url="/pages/book1/1/main"></i-cell>
+    <i-cell title="第二章" is-link url="/pages/book1/2/main"></i-cell>
+    <i-cell title="第三章" is-link url="/pages/book1/3/main"></i-cell>
+    <i-cell title="第四章" is-link url="/pages/book1/4/main"></i-cell>
+    <i-cell title="第五章" is-link url="/pages/book1/5/main"></i-cell>
+    <i-cell title="第六章" is-link url="/pages/book1/6/main"></i-cell>
+    <i-cell title="第七章" is-link url="/pages/book1/7/main"></i-cell>
+    <i-cell title="第八章" is-link url="/pages/book1/8/main"></i-cell>
     <p>蚩尤的后代</p>
-    <i-cell title="第一章" is-link url="/pages/book5/mulu/9/main"></i-cell>
-    <i-cell title="第二章" is-link url="/pages/book5/mulu/10/main"></i-cell>
-    <i-cell title="第三章" is-link url="/pages/book5/mulu/11/main"></i-cell>
-    <i-cell title="第四章" is-link url="/pages/book5/mulu/12/main"></i-cell>
-    <i-cell title="第五章" is-link url="/pages/book5/mulu/13/main"></i-cell>
-    <i-cell title="第六章" is-link url="/pages/book5/mulu/14/main"></i-cell>
+    <i-cell title="第一章" is-link url="/pages/book1/9/main"></i-cell>
+    <i-cell title="第二章" is-link url="/pages/book1/10/main"></i-cell>
+    <i-cell title="第三章" is-link url="/pages/book1/11/main"></i-cell>
+    <i-cell title="第四章" is-link url="/pages/book1/12/main"></i-cell>
+    <i-cell title="第五章" is-link url="/pages/book1/13/main"></i-cell>
+    <i-cell title="第六章" is-link url="/pages/book1/14/main"></i-cell>
     <p>我本是树</p>
-    <i-cell title="第一章" is-link url="/pages/book5/mulu/15/main"></i-cell>
-    <i-cell title="第二章" is-link url="/pages/book5/mulu/16/main"></i-cell>
-    <i-cell title="第三章" is-link url="/pages/book5/mulu/17/main"></i-cell>
+    <i-cell title="第一章" is-link url="/pages/book1/15/main"></i-cell>
+    <i-cell title="第二章" is-link url="/pages/book1/16/main"></i-cell>
+    <i-cell title="第三章" is-link url="/pages/book1/17/main"></i-cell>
    <p>西域喀什</p>
-    <i-cell title="第一章" is-link url="/pages/book5/mulu/18/main"></i-cell>
-    <i-cell title="第二章" is-link url="/pages/book5/mulu/19/main"></i-cell>
-    <i-cell title="第三章" is-link url="/pages/book5/mulu/20/main"></i-cell>
-    <i-cell title="第四章" is-link url="/pages/book5/mulu/21/main"></i-cell>
+    <i-cell title="第一章" is-link url="/pages/book1/18/main"></i-cell>
+    <i-cell title="第二章" is-link url="/pages/book1/19/main"></i-cell>
+    <i-cell title="第三章" is-link url="/pages/book1/20/main"></i-cell>
+    <i-cell title="第四章" is-link url="/pages/book1/21/main"></i-cell>
     
     </i-cell-group>
     </view>
